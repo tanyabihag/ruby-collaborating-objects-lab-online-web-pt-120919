327054ebfbc4ca7ff2c91c1b4b370c0586342477
Tanya Bihag <tbihag@gmail.com>
2020-01-21 04:07:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,4 +19,10 @@ def self.new_by_filename(file)
     song.artist_name = artist_name
     song
     end
+    
+     def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
 end
