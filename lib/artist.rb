374ec5e80c8f6f8a7b82741101f4c8504aa7a51e
Tanya Bihag <tbihag@gmail.com>
2020-01-21 04:11:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    save
+    @@all << self
   end
 
   def self.all
@@ -35,9 +35,6 @@ class Artist
     songs.each { |song| puts song.name }
   end
 
-  def save
-    @@all << self
-  end
 
 def songs
     Song.all.select {|song| song.artist == self}
