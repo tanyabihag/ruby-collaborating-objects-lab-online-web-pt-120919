df914ed5ed1264acea220b644a0214c8742132bf
Tanya Bihag <tbihag@gmail.com>
2020-01-21 04:07:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,45 @@
-class Artist 
+class Artist
+  attr_accessor :name, :songs
 
-attr_accessor :name, :songs
+  @@all = []
 
-@@all = []
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
 
-def initialize(name)
-  @name = name 
-  @@all << self
-  @songs = []
+  def self.all
+    @@all
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
+  end
 end 
-
-def self.all 
-  @@all
-end 
-
-def songs 
-  @songs
-end
-
-end
