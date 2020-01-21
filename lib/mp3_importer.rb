fa036ab12e1a6b28a5775ee2af3c25a93e525aaa
Tanya Bihag <tbihag@gmail.com>
2020-01-21 03:55:33 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,3 +1,17 @@
 class MP3Importer
+ 
+ attr_accessor :path, :file_field
+ 
+ def initialize(path)
+   @path = path 
+  end
   
+  def files(files)
+    @files = Dir.entries(path).grep(/.*\.mp3/)
+  end 
+  
+  def import
+    @files.each {|file| Song.new_by_filename(file)}
+  end
+end
   
